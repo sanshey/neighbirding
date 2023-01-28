a044686cb3dc6e010a88122270bd827cef2506cc
sanshey <sanshey@gmail.com>
2023-01-28 16:12:08 +0530

    pic-css-installed

--- neighbird/src/routes/+page.svelte
+++ neighbird/src/routes/+page.svelte
@@ -1,3 +1,14 @@
 <h1>Welcome to Neighbird</h1>
 <h2> A new website</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<nav>
+    <ul>
+      <li><strong> <a href="https://www.neighbirding.com">Neighbird</a> </strong></li>
+    </ul>
+    <ul>
+      <li><a href="https://www.youtube.com/@neighbirding">Youtube</a></li>
+      <li><a href="https://www.instagram.com/neighbirding/">Instagram</a></li>
+      <li><a href="https://twitter.com/neighbirding">Twitter</a></li>
+    </ul>
+  </nav>
