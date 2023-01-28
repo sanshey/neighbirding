43096304f1d73183e9559aaf9be09d4e67c2daf1
sanshey <sanshey@gmail.com>
2023-01-28 17:38:52 +0530

    rough-template

--- neighbird/src/routes/+page.svelte
+++ neighbird/src/routes/+page.svelte
@@ -1,8 +1,8 @@
-<h1>Welcome to Neighbird</h1>
+<!-- <h1>Welcome to Neighbird</h1>
 <h2> A new website</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
-<nav>
+<nav class="container-fluid">
     <ul>
       <li><strong> <a href="https://www.neighbirding.com">Neighbird</a> </strong></li>
     </ul>
@@ -12,3 +12,68 @@
       <li><a href="https://twitter.com/neighbirding">Twitter</a></li>
     </ul>
   </nav>
+
+  <hr>
+
+<main class="container">
+  
+  <section id="about"></section>
+    <hgroup>
+
+      <h1>This is a heading</h1>
+      <h2>And a little slug</h2>
+    
+    </hgroup>
+
+    <p>This is paragraph. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa obcaecati cum saepe voluptatum quas? Voluptate, sit. Consequuntur in iste et esse quae reprehenderit accusantium dignissimos hic dolore, commodi facere perspiciatis? </p>
+
+    <!-- <div class="container">
+
+      <img src="favicon.png" alt="background" />
+
+      <p>
+        
+        neighbird/static/favicon.png
+      </p>
+
+    </div> -->
+  
+    <div class="grid">
+
+      <article>
+        <p><img src="img/127739202_839251086849043_6045538471618887742_n.jpg" alt=""></p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus fuga quis eaque alias repudiandae facere eligendi harum deleniti tenetur similique dolore eos aspernatur inventore corrupti molestias distinctio, numquam quo ratione!</p>
+      </article>
+
+      <article>
+
+        <p><img src="img/127260863_3615531218530766_4723419181862680163_n.jpg" alt="">
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus fuga quis eaque alias repudiandae facere eligendi harum deleniti tenetur similique dolore eos aspernatur inventore corrupti molestias distinctio, numquam quo ratione!
+        </p>
+        
+      </article>
+
+      <article>
+        <p>
+          <img src="img/143369537_441463780313024_4999058208382829039_n.jpg" alt="">
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus fuga quis eaque alias repudiandae facere eligendi harum deleniti tenetur similique dolore eos aspernatur inventore corrupti molestias distinctio, numquam quo ratione!
+        </p>
+        
+      </article>
+
+    </div>
+
+</main>
+
+<hr>
+
+<footer class="container">
+  <p>
+    <small>A project by <a href="http://inoldnews.com">In Old News LLC</a> • <a href="https://github.com/picocss/examples/tree/master/company/">Source code</a></small>
+
+  </p>
+</footer>
+
